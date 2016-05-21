6d451c43838a5e47f213e69b104a6ec38700f086
zenoamaro <zenoamaro@gmail.com>
2016-05-21 21:03:20 +0200

    Initial codemirror integration

--- src/renderer/controls/CodeEditor.jsx
+++ src/renderer/controls/CodeEditor.jsx
@@ -1,40 +1,40 @@
 /* @flow weak */
 import React from 'react';
-import Look, {StyleSheet} from 'react-look';
 import Component from './Component';
+import Codemirror from 'react-codemirror';
+require('codemirror/mode/javascript/javascript');
+require('codemirror/lib/codemirror.css');
 
 
-@Look
 export default class CodeEditor extends Component {
 
 	static propTypes = {
 		onChange: React.PropTypes.func,
 		value: React.PropTypes.string,
-		style: React.PropTypes.object,
 	};
+
+	getOptions() {
+		return {
+			mode: 'javascript',
+			indentUnit: 2,
+			smartIndent: true,
+			electricChars: true,
+			lineWrapping: true,
+			lineNumbers: true,
+			fixedGutter: true,
+			lineWiseCopyCut: true,
+			autofocus: true,
+		};
+	}
 
 	render() {
 		return (
-			<textarea
-				className={CodeEditor.styles.editor}
+			<Codemirror
 				onChange={this.props.onChange}
-				styles={this.props.style}
 				value={this.props.value}
+				options={this.getOptions()}
 			/>
 		);
 	}
-
-	static styles = StyleSheet.create({
-		editor: {
-			flex: 1,
-			alignSelf: 'stretch',
-			padding:' 0',
-			fontFamily: 'menlo, monaco, monospace',
-			fontSize:' 11px',
-			fontWeight:' 100',
-			lineHeight:' 1.4',
-			border:' none',
-		},
-	});
 
 }
